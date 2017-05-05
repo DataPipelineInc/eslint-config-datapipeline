c9e8bef48ed6eb8491899821a1d011b32b2d3b74
VincentBel <buaazqh@gmail.com>
2017-05-05 18:28:54 +0800

    chore: add script to run lint rule for  project

---
+++ run-lint-for-repo.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# testing new rules updated from upstream(airbnb) for our project
+
+GIT_REPO_NAME="$1"
+
+if [[ $GIT_REPO_NAME == "" ]]; then
+  echo "You need to pass an github repo name argument"
+  exit 1
+fi
+
+TEMP_DIR="__temp__"
+
+mkdir -p $TEMP_DIR
+
+cd $TEMP_DIR
+
+if [[ ! -d "$GIT_REPO_NAME" ]]; then
+  git clone "git@github.com:DataPipelineInc/$GIT_REPO_NAME.git"
+fi
+
+cd $GIT_REPO_NAME
+git pull
+
+npm install
+npm install ../../
+
+npm run lint
